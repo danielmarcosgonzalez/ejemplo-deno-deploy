a0bb8d2392e83f21f709b8cdba4881d3611b6a5e
danielmarcosgonzalez <125558887+danielmarcosgonzalez@users.noreply.github.com>
2023-10-27 11:08:32 +0200

    update main

--- deno_example/main.ts
+++ deno_example/main.ts
@@ -10,6 +10,11 @@ app
     res.status(404).send("Hola mundo");
   })
 
+  .get("/hola",async(req: Request, res: Response)=>{
+    res.status(404).send("que tal estamos");
+  })
+
+
   app.listen(3000, () => {
     console.log("Server listening on port 3000");
   });
